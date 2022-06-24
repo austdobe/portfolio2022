5bfec866c8effc04d8304cc9b8da7798ce597b3c
Austin <austin.dober@gmail.com>
2022-06-24 16:47:26 -0400

    fixing refresh issue when on a path

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import LandingView from "../views/LandingView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  mode: 'history',
   routes: [
     {
       path: "/",
