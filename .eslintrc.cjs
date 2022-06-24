f6cea132c897fc0c6af2f7e91286ccd52e0f98bb
Austin <austin.dober@gmail.com>
2022-06-24 14:07:58 -0400

    adding base files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,10 +8,4 @@ module.exports = {
     "eslint:recommended",
     "@vue/eslint-config-prettier",
   ],
-  overrides: [
-    {
-      files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
-  ],
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LandingView from "../views/LandingView.vue";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "Landing",
-      component: LandingView,
+      name: "home",
+      component: HomeView,
     },
     {
       path: "/about",
-      name: "About",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/work",
-      name: "Work",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/WorkView.vue"),
     },
   ],
 });
