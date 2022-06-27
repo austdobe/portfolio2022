902b908163d94ab872ed7061d7d4c20da00ad884
Austin <austin.dober@gmail.com>
2022-06-27 11:24:44 -0400

    making changes to nav history

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LandingView from "../views/LandingView.vue";
+import { routes } from './routes'
 
+const url = import.meta.env.BASE_URL
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(url),
   mode: 'history',
   scrollBehavior: function(to, from, savedPosition) {
     if (savedPosition) {
@@ -14,59 +15,8 @@ const router = createRouter({
     } else {
         return { x: 0, y: 0 }
     }
-},
-  routes: [
-    {
-      path: "/",
-      name: "Landing",
-      component: LandingView,
-      meta: {
-        hideNavbar: true,
-      }
-    },
-    {
-      path: "/about",
-      name: "About",
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/work",
-      name: "Work",
-      component: () => import("../views/WorkView.vue"),
-    },
-    {
-      path: "/work/wagsWithFriends",
-      beforeEnter() {
-        location.href = `https://project2-10-2019.herokuapp.com/`
-      },
-    },
-    {
-      path: "/work/:redirect",
-      props: route => ({redirectURL: `${route.params.redirect}`}),
-      beforeEnter(redirectURL) {
-        const params = redirectURL.params.redirect
-        location.href = `http://austindober.com/${params}`
-      },
-    },
-    {
-      path: "/resume",
-      name: "Resume",
-      component: () => import("../views/ResumeView.vue"),
-    },
-    {
-      path: "/contact",
-      name: "Contact",
-      component: () => import("../views/ContactView.vue"),
-    },
-    {
-      path: "/404",
-      component: () => import('../views/404.vue')
-    },
-    {
-      path: "/:catchAll(.*)",
-      redirect: "/",
-    },
-  ],
+  },
+  routes
 });
 
 export default router;
---
+++ src/router/routes.js
@@ -0,0 +1,54 @@
+
+
+export const routes = [
+    {
+      path: "/",
+      name: "Landing",
+      component: () => import("../views/LandingView.vue"),
+      meta: {
+        hideNavbar: true,
+      }
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/work",
+      name: "Work",
+      component: () => import("../views/WorkView.vue"),
+    },
+    {
+      path: "/work/wagsWithFriends",
+      beforeEnter() {
+        location.href = `https://project2-10-2019.herokuapp.com/`
+      },
+    },
+    {
+      path: "/work/:redirect",
+      props: route => ({redirectURL: `${route.params.redirect}`}),
+      beforeEnter(redirectURL) {
+        const params = redirectURL.params.redirect
+        location.href = `http://austindober.com/${params}`
+      },
+    },
+    {
+      path: "/resume",
+      name: "Resume",
+      component: () => import("../views/ResumeView.vue"),
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      component: () => import("../views/ContactView.vue"),
+    },
+    {
+      path: "/404",
+      component: () => import('../views/404.vue')
+    },
+    {
+      path: "/:catchAll(.*)",
+      redirect: "portfolio2022/",
+    },
+  ]
