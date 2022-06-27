c54a7002855fd71cbc38b43758362b0682f94a57
Austin <austin.dober@gmail.com>
2022-06-27 11:07:41 -0400

    Updates

---
+++ assets/404.bbdc8e73.js
@@ -0,0 +1,1 @@
+import{_ as e,o as _,c}from"./index.cf527641.js";const r={};function t(a,n){return _(),c("h1",null,"Opps! Wrong turn!")}var f=e(r,[["render",t],["__scopeId","data-v-7fb7bc7f"]]);export{f as default};
