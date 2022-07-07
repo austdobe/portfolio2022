a5a21f2996abcb391dcb85d46b2bf7b7badbb57c
Austin <austin.dober@gmail.com>
2022-07-07 16:22:33 -0400

    Updates

---
+++ assets/NotFound.1adf880a.js
@@ -0,0 +1,1 @@
+import{_ as e,c as _,o}from"./index.494a961f.js";const t={};function c(n,r){return o(),_("h1",null,"Opps! Wrong turn!")}var d=e(t,[["render",c],["__scopeId","data-v-08f6d03c"]]);export{d as default};
